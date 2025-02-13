afdad17c3149537db2470f4e40fba98cb11e6421
Dalk <102332227+Dalk21@users.noreply.github.com>
2025-02-12 21:48:10 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# i2.js
+
+someone pls writ4e docs lol 
---
+++ index.js
@@ -0,0 +1,7 @@
+const exec = require("./modules/exec")
+const playlistManager = require("./modules/playlistManager")
+const sys = require("./modules/sys")
+const data = require("./modules/data")
+const bundle = require("./modules/bundle")
+
+module.exports = {exec,playlistManager,sys,data,bundle}
